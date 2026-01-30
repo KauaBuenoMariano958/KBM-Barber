8e7a20fe4300817f83eb6069a1252ae38a4e47b9
Kaua Bueno Mariano <kauabuenomariano@gmail.com>
2026-01-29 23:34:30 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import { Button } from './-components/ui/button'
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>Bem-vindo ao Sistema de Agendamento de Barbearia</h1>
-    </div>
+    <Button>Test</Button>
   )
 }
 
