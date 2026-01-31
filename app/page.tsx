2329a6013580b8aec9a42be8c4ae67c97e84035c
Kaua Bueno Mariano <kauabuenomariano@gmail.com>
2026-01-31 00:19:39 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import { Card, CardContent } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
 
 const Home = () => {
   return (
@@ -28,6 +31,32 @@ const Home = () => {
             alt="Agende nos melhores com FSW Barber"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/*Esquerda*/}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit rounded-3xl bg-indigo-700 text-white">
+                Confirmado
+              </Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/*Direita*/}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm font-semibold">08:00 AM</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
