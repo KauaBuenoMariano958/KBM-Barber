b85e4d125c48e3792cd29c44dd2e32bd0544491b
Kaua Bueno Mariano <kauabuenomariano@gmail.com>
2026-01-30 23:29:54 -0300

    feat: add Header components and banner

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - [x] Seeding do banco (colocar dados)
 - [x] Introdução ao Next.js
 - [x] Tailwind e Shadcn
-- [] Git hooks
+- [x] Git hooks
 
 
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
 import React from "react"
-import { Button } from "./_components/ui/button"
-
-const name = "test"
-console.log(name)
+import Header from "@/components/header"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
   return (
     <div>
-      <Button>Test</Button>
-      <h1>{name}</h1>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Kauã</h2>
+        <p>Segunda-feira, 05 de agosto. </p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça a sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agende nos melhores com FSW Barber"
+          />
+        </div>
+      </div>
     </div>
   )
 }
