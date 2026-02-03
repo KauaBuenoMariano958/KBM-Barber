c9346e2c202e551d17fd7050b1d9e1131947d10c
Kaua Bueno Mariano <kauabuenomariano@gmail.com>
2026-02-02 23:43:12 -0300

    feat: add quick search

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,11 @@ import BarbershopItem from "@/components/barbershop-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({}) //por serum server-components eu consigo acessar o banco de dados diretamente aqui
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
 
   return (
     <div>
@@ -20,10 +25,54 @@ const Home = async () => {
         <h2 className="text-xl font-bold">Olá, Kauã</h2>
         <p>Segunda-feira, 05 de agosto. </p>
 
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça a sua busca" />
-          <Button>
+          <Input className="rounded-xl" placeholder="Faça a sua busca" />
+          <Button className="rounded-xl">
             <SearchIcon />
+          </Button>
+        </div>
+
+        {/* BUSCA RÁPIDA */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button className="gap-2" variant="secondary">
+            <Image src="/cabelo.svg" width={16} height={16} alt="Cabelo" />
+            Cabelo
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="/barba.svg" width={16} height={16} alt="Barba" />
+            Barba
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/acabamento.svg"
+              width={16}
+              height={16}
+              alt="Acabamento"
+            />
+            Acabamento
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/hidratacao.svg"
+              width={16}
+              height={16}
+              alt="Hidratação"
+            />
+            Hidratação
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/sobrancelha.svg"
+              width={16}
+              height={16}
+              alt="Sobrancelha"
+            />
+            Sobrancelha
           </Button>
         </div>
 
@@ -76,7 +125,26 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card className="mt-5 rounded-t-lg bg-gray-900">
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
