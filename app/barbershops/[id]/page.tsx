4b4fe97fa9c617dfb2b884cb6ed17d5735d9fd17
Kaua Bueno Mariano <kauabuenomariano@gmail.com>
2026-02-03 20:14:21 -0300

    feat: add initial barbershop page

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,81 @@
+import { db } from "@/app/_lib/prisma"
+import React from "react"
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import { ChevronLeftIcon, MapIcon, MenuIcon, StarIcon } from "lucide-react"
+import Link from "next/link"
+import { notFound } from "next/navigation"
+
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  //Estou pegando o ID pela prop params e estou usando esse ID para fazer a query no banco de dados e pela query eu tenho o banco de dados do barbearia especifica
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+
+  if (!barbershop) {
+    return notFound()
+  }
+
+  return (
+    /* IMAGEM */
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image
+          src={barbershop?.imageUrl}
+          alt={barbershop?.name}
+          className="object-cover"
+          fill
+        />
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute left-4 top-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute right-4 top-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+
+      {/* CONTEÚDO DA BARBEARIA */}
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+        <div className="mb-2 flex items-center gap-2">
+          <MapIcon className="text-indigo-700" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <StarIcon className="fill-indigo-700 text-indigo-700" size={18} />
+          <p className="text-sm">5,0 (889 avaliações)</p>
+        </div>
+      </div>
+
+      {/* DESCRIÇÃO */}
+      <div className="space-y-2 border-b border-solid p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+        <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
