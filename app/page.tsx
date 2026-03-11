d95f6c26d799894ece140107ea16cf13304a9e38
Kaua Bueno Mariano <kauabuenomariano@gmail.com>
2026-03-11 00:17:06 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ import Search from "@/components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { ptBR } from "date-fns/locale"
+import { format } from "date-fns"
 
 const Home = async () => {
   try {
@@ -51,7 +53,16 @@ const Home = async () => {
           <h2 className="text-xl font-bold">
             Olá, {session?.user ? session.user.name : "bem vindo"}!
           </h2>
-          <p>Segunda-feira, 05 de agosto. </p>
+          <p>
+            {(() => {
+              const formattedDate = format(new Date(), "EEEE, dd 'de' MMMM", {
+                locale: ptBR,
+              })
+              return (
+                formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)
+              )
+            })()}
+          </p>
 
           {/* BUSCA */}
           <div className="mt-6">
